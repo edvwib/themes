6b8bdc39a77c9dde2c883ec48aa610ac286b7c39
edvwib <edde275@gmail.com>
2018-02-27 11:45:50 +0100

    add base for new yrgo theme

---
+++ yrgo/single.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+<?php if (have_posts()): ?>
+
+    <?php while (have_posts()): the_post(); ?>
+
+
+      <div class="row">
+        <h2 class="col-12">
+          <?php the_title(); ?>
+        </h2>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <?php the_content(); ?>
+        </div>
+      </div>
+
+    <?php endwhile; ?>
+
+<?php endif; ?>
+
+<?php get_footer(); ?>
