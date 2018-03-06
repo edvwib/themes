f336fd65b356fd4df71a8d08ea18c0410e69592d
edvwib <edde275@gmail.com>
2018-03-06 10:02:13 +0100

    change css/script links to worpress style

--- yrgo/functions.php
+++ yrgo/functions.php
@@ -1,13 +1,18 @@
 <?php
 
 function isActive (string $page) {
-    if (stripos($_SERVER['REQUEST_URI'],$page)) {
+    if (stripos($_SERVER['REQUEST_URI'], $page))
         return 'active';
-    }
-
     return '';
 }
 
 add_action('wp_enqueue_scripts', function () {
+    wp_deregister_script('jquery');
 
+    wp_enqueue_style('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css');
+    wp_register_script('jquery', 'https://code.jquery.com/jquery-3.2.1.slim.min.js', '', '', true);
+    wp_register_script('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js', '', '', true);
+
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('bootstrap');
 });
